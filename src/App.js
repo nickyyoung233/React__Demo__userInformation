31daae067077d30127c901d7facefcc48e005b17
16684814105 <402150786@qq.com>
2023-07-26 09:14:21 +0800

    base structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import React from 'react';
-
+import React from "react";
 
 function App() {
   return (
-    <div>
-
+    <div className="card">
+      <form className="card form">
+        <label htmlFor="username">Username</label>
+        <input type="text" id="username" />
+        <label htmlFor="age">Age(Years)</label>
+        <input type="number" min="0" id="age" />
+        <button className="button" type="submit">
+          Add User
+        </button>
+      </form>
+      <ul className="userList">
+        <li className="userItem">Some Stuff</li>
+        <li className="userItem">Some Stuff</li>
+      </ul>
     </div>
   );
 }
