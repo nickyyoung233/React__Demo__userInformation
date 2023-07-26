4dd18b7bc1409832a7fb1a0751a0ae3bba63bad4
16684814105 <402150786@qq.com>
2023-07-26 09:53:28 +0800

    adjust structure & styles

--- src/App.js
+++ src/App.js
@@ -2,20 +2,29 @@ import React from "react";
 
 function App() {
   return (
-    <div className="card">
-      <form className="card form">
-        <label htmlFor="username">Username</label>
-        <input type="text" id="username" />
-        <label htmlFor="age">Age(Years)</label>
-        <input type="number" min="0" id="age" />
-        <button className="button" type="submit">
-          Add User
+    <div>
+      <div className="card">
+        <form className="card form">
+          <label htmlFor="username">Username</label>
+          <input type="text" id="username" />
+          <label htmlFor="age">Age(Years)</label>
+          <input type="number" min="0" id="age" />
+          <button className="button" type="submit">
+            Add User
+          </button>
+        </form>
+        <ul className="userList">
+          <li className="userItem">Some Stuff</li>
+          <li className="userItem">Some Stuff</li>
+        </ul>
+      </div>
+      <div className="error card">
+        <header className="errorHeader">Invalid Input</header>
+        <p>Please enter a valid name and age(non-empty values).</p>
+        <button className="button" type="button">
+          Okay
         </button>
-      </form>
-      <ul className="userList">
-        <li className="userItem">Some Stuff</li>
-        <li className="userItem">Some Stuff</li>
-      </ul>
+      </div>
     </div>
   );
 }
